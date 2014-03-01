bda8b02cea76cdfa04e27e6391b1554c1afb3d9c
Brice Lambson <bricelam@outlook.com>
2014-03-01 11:59:34 -0800

    Add DbNull

---
+++ src/System.Data.Common/DbNull.cs
@@ -0,0 +1,37 @@
+﻿#if NET45
+
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(DBNull))]
+
+#else
+
+namespace System
+{
+    /// <summary>
+    /// Represents a nonexistent value. This class cannot be inherited.
+    /// </summary>
+    public sealed class DBNull
+    {
+        /// <summary>
+        /// Represents the sole instance of the <see cref="DBNull" /> class.
+        /// </summary>
+        public static readonly DBNull Value = new DBNull();
+
+        private DBNull()
+        {
+        }
+
+        /// <summary>
+        /// Returns an empty string (<see cref="String.Empty" />).
+        /// </summary>
+        /// <returns>An empty string (<see cref="String.Empty" />).</returns>
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+}
+
+#endif
