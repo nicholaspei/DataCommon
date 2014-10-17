c39090fc0a941c56bcd7366ebdfb69de87fdfb6e
Brice Lambson <bricelam@microsoft.com>
2014-10-17 11:46:08 -0700

    Type forward on aspnet50

--- src/System.Data.Common/IsolationLevel.cs
+++ src/System.Data.Common/IsolationLevel.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#if NET451
+#if NET451 || ASPNET50
 
 using System.Data;
 using System.Runtime.CompilerServices;
